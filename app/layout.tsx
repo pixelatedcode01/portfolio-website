b2931d32ecd8d300ef22a1815999caaf94c8cf75
Naman Rastogi <namanrastogi.materiox@gmail.com>
2025-02-02 13:09:14 +1300

    Adds Next Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "material-symbols";
 import NavigationBar from "@/components/ui/navigationbar";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const manrope = Manrope({
   variable: "--font-manrope-sans",
@@ -30,11 +31,12 @@ export default function RootLayout({
       <body
         className={`${manrope.className} ${geistMono.variable} antialiased`}
       >
-        <Toaster/>
+        <Toaster />
         <NavigationBar />
         <main className="min-h-full h-fit w-full flex justify-center bg-gradient-to-tr from-slate-50 to-cyan-100">
           <div className="lg:w-[50%] w-full h-full flex flex-col pt-20 mb-8 px-8 gap-4">
             {children}
+            <SpeedInsights />
           </div>
         </main>
       </body>
